597aac3256cfd42d80af7753c43b06af1c70afb5
Your NameMustafa-Kok <Mustafa.andoo99@gmail.com>
2020-12-18 12:59:03 +0300

    fixed

--- index.js
+++ index.js
@@ -12,19 +12,7 @@ var server = app.listen(3000, function () {
 app.use(express.static("public"));
 
 // Socket setup & pass server
-var io = socket(server);
 
-io.on("connection", function (socket) {
-  console.log("user connected");
-
-  socket.on("disconnect", function () {
-    console.log("user disconnected");
-  });
-  socket.on("message", function (msg) {
-    console.log("message: " + msg);
-    io.emit("message", msg);
-  });
-});
 app.use((req, res, next) => {
-  return errRes(res, "404 Not found");
+  return res.json({ msg: "404 Not found" });
 });
